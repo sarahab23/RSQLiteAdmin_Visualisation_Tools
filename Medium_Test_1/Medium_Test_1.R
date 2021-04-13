0b9b97899d3090ee8f5e9eb9bbd26d3b61966952
Sarah Abraham <44671982+sarahab23@users.noreply.github.com>
2021-04-13 15:19:50 +0530

    Added Medium_Test_1.R

---
+++ Medium_Test_1/Medium_Test_1.R
@@ -0,0 +1,12 @@
+library(ggplot2)
+theme_set(theme_minimal())
+
+head(economics)
+
+# For data preparation
+library("tidyverse")
+df <- economics%>%select(date, psavert, uempmed)%>%gather(key = "variables", value = "value", -date)
+head(df)
+
+# Data Visualization
+> ggplot(df, aes(x = date, y = value)) + geom_line(aes(color = variables)) + scale_color_manual(values = c("yellow", "green"))
